c5c763fd4612aee5fe03bbcb3fa21543ab1b8ca5
EFCORITI <106176816+EFCORITI@users.noreply.github.com>
2022-08-11 13:47:17 +0430

    Add files via upload

---
+++ filteringRubika.py
@@ -0,0 +1,135 @@
+# usr/bin/python
+# coded by ali
+# script filtering
+
+import random
+from colorama import Fore
+import time
+import os
+
+# =========================================
+os.system("pip install colorama")
+os.system("clear")
+# =========================================
+print(Fore.GREEN)
+print(""" 
+
+    _______ ____            _                ____        __    _ __        
+   / ____(_) / /____  _____(_)___  ____ _   / __ \__  __/ /_  (_) /______ _
+  / /_  / / / __/ _ \/ ___/ / __ \/ __ `/  / /_/ / / / / __ \/ / //_/ __ `/
+ / __/ / / / /_/  __/ /  / / / / / /_/ /  / _, _/ /_/ / /_/ / / ,< / /_/ / 
+/_/   /_/_/\__/\___/_/  /_/_/ /_/\__, /  /_/ |_|\__,_/_.___/_/_/|_|\__,_/  
+                                /____/                                     
+ """)
+print(Fore.LIGHTBLUE_EX)
+print(""" 
+.----..----..---.  .----. .----. .-. .---. .-.
+| {_  | {_ /  ___}/  {}  \| {}  }| |{_   _}| |
+| {__ | |  \     }\      /| .-. \| |  | |  | |
+`----'`-'   `---'  `----' `-' `-'`-'  `-'  `-'
+ """)
+print(Fore.CYAN+"		CODED BY ALI")
+print(Fore.LIGHTGREEN_EX+ """		Id Telegram : @efcoriti """)
+
+# =========================================
+
+print("\n\n")
+print(Fore.RED+" \t1 - "+Fore.YELLOW+"code filtering account")
+print(Fore.RED+" \t2 - "+Fore.YELLOW+"code filtering group")
+print(Fore.RED+" \t3 - "+Fore.YELLOW+"code filtering channel")
+
+# =========================================
+
+print("\n\n")
+code = input(Fore.CYAN+" ["+Fore.RED+"+"+Fore.CYAN+"]"+ Fore.BLUE+" enter the number for filtering rubika : "+Fore.GREEN+"")
+print("\n\n")
+# =========================================
+if code == '1':
+	print(Fore.BLUE+" •")
+	time.sleep(0.5)
+	print(Fore.CYAN+" ••")
+	time.sleep(0.5)
+	print(Fore.GREEN+" •••")
+	time.sleep(0.5)
+	print(Fore.LIGHTBLUE_EX+" ••••")
+	time.sleep(0.5)
+	print(Fore.LIGHTCYAN_EX+" •••••")
+	time.sleep(0.5)
+	print(Fore.LIGHTGREEN_EX+" ••••••")
+	time.sleep(0.5)
+	print(Fore.LIGHTMAGENTA_EX+" •••••••")
+	time.sleep(0.5)
+	print(Fore.LIGHTRED_EX+" ••••••••")
+	time.sleep(0.5)
+	print(Fore.LIGHTYELLOW_EX+" •••••••••")
+	time.sleep(0.5)
+	print(Fore.MAGENTA+" ••••••••••")
+	time.sleep(0.5)
+	print("\n")
+	print(Fore.RED+"Code Filtering Account => ")
+	time.sleep(1)
+	print(Fore.LIGHTWHITE_EX)
+	g = ["Dxprit.erorr18.filtering.spam.server.erorr.yftt.com.https://bit.ly/3fX8ljZ.19 20:38:10.053.http://Dxprit.ngopb.hackers65.78098755434343434766.infect.in.https://s4.uupload.ir/files/screenshot_۲۰۲۱۱۰۰۸-۲۳۰۵۰۳_l5zo.png.http://Dxprit.ngopb.hackers65.780999999998755766.infect.in.sxs.photo.in.rubika.https://bit.ly/3ild93Ljz.12:67/https://s6.uupload.ir/files/img_20220421_122708_504_zx3o.jpg" , "Dxprit.erorr18.filtering.spam.server.erorr.yftt.com.https://bit.ly/3fX8ljZ.19 20:38:10.053.http://Dxprit.ngopb.hackers65.78098755434343434766.infect.in.https://s4.uupload.ir/files/screenshot_۲۰۲۱۱۰۰۸-۲۳۰۵۰۳_l5zo.png.http://Dxprit.ngopb.hackers65.780999999998755766.infect.in.sxs.photo.in.rubika","https://s6.uupload.ir/files/img_20220421_122708_504_zx3o.jpg/26:78:00:11.tering.spam.server.erorr.yftt.com.https://bit.ly/3fX8ljZ.19 20:38:10.053.sxs.film","http://uupload.ir/files/screenshot_۲۰۲۱۱۰۰۸-۲۳۰۵۰۳_l5zo.png.http://Dxprit.ngopb.hackers65.780999999998755766.infect.in.sxs.photo.in.rubika/https://bit.ly/3ild93Ljz","filtering.rubika/porn.hub/https://bit.ly/3ild93Ljz/https://s6.uupload.ir/files/img_20220421_122708_504_zx3o.jpg/film.vedio/xxx.21.23.34","http://Dxprit.ngopb.hackers.65infect.in.sxs.photo.in.rubika/https://bit.ly/3ild93Ljzhttp://uupload.ir/files/screenshot_۲۰۲۱۱۰۰۸-۲۳۰۵۰۳_l5zo.png.http://Dxprit.ngopb.780999999998755766.infect.in.sxs.photo.in.rubika/33.23.54.0:00","http://Dxprit.ngopb.hackers65.78098755434343434766.infect.in.https://pornhub.com.32::51.rub/https://bit.ly/3ild93Ljz"]
+	print(random.choice(g))
+	print(Fore.GREEN+"\n\n\n\t CODE "+Fore.WHITE+"FILTERING"+Fore.RED+ " ACCOUNT ")
+
+# =========================================
+if code == '2':
+	print(Fore.BLUE+" •")
+	time.sleep(0.5)
+	print(Fore.CYAN+" ••")
+	time.sleep(0.5)
+	print(Fore.GREEN+" •••")
+	time.sleep(0.5)
+	print(Fore.LIGHTBLUE_EX+" ••••")
+	time.sleep(0.5)
+	print(Fore.LIGHTCYAN_EX+" •••••")
+	time.sleep(0.5)
+	print(Fore.LIGHTGREEN_EX+" ••••••")
+	time.sleep(0.5)
+	print(Fore.LIGHTMAGENTA_EX+" •••••••")
+	time.sleep(0.5)
+	print(Fore.LIGHTRED_EX+" ••••••••")
+	time.sleep(0.5)
+	print(Fore.LIGHTYELLOW_EX+" •••••••••")
+	time.sleep(0.5)
+	print(Fore.MAGENTA+" ••••••••••")
+	time.sleep(0.5)
+	print("\n")
+	print(Fore.RED+"Code Filtering Group => ")
+	time.sleep(1)
+	print(Fore.LIGHTWHITE_EX)
+	p = ["rubika.filtering.photo/filtering.hacking.sxs.xxx.porn/12.23.00:22.https://bit.ly/3ild93Ljz.rub.xxx.video.in.rubika/xxx" , "https://s6.uupload.ir/files/img_20220421_122708_504_zx3o.jpg.http://Dxprit.ngopb.780999999998755766.21.20.76:11/infect.in.sxs.photo.in.rub","https://s6.uupload.ir/files/img_20220421_122708_504_zx3o.jpg/https://bit.ly/3ild93Ljz.video.photo.sxs","files.video.picture.photo.xxx.in.rubika.https://pornhub.com/https://bit.ly/3ild93Ljz/rub.sxs","https://s6.uupload.ir/files/img_20220421_122708_504_zx3o.jpg/in.rubika.sxs.xxx.photo.filtering","sxs.xxx.porn.rubika.video.picture.rub/files.https://s6.uupload.ir/files/img_20220421_122708_504_zx3o.jpg"]
+	print(random.choice(p))
+	print(Fore.GREEN+"\n\n\n\t CODE "+Fore.WHITE+"FILTERING"+Fore.RED+ " GROUP ")
+# =========================================
+
+if code == '3':
+	print(Fore.BLUE+" •")
+	time.sleep(0.5)
+	print(Fore.CYAN+" ••")
+	time.sleep(0.5)
+	print(Fore.GREEN+" •••")
+	time.sleep(0.5)
+	print(Fore.LIGHTBLUE_EX+" ••••")
+	time.sleep(0.5)
+	print(Fore.LIGHTCYAN_EX+" •••••")
+	time.sleep(0.5)
+	print(Fore.LIGHTGREEN_EX+" ••••••")
+	time.sleep(0.5)
+	print(Fore.LIGHTMAGENTA_EX+" •••••••")
+	time.sleep(0.5)
+	print(Fore.LIGHTRED_EX+" ••••••••")
+	time.sleep(0.5)
+	print(Fore.LIGHTYELLOW_EX+" •••••••••")
+	time.sleep(0.5)
+	print(Fore.MAGENTA+" ••••••••••")
+	time.sleep(0.5)
+	print("\n")
+	print(Fore.RED+"Code Filtering Channel => ")
+	time.sleep(1)
+	print(Fore.LIGHTWHITE_EX)
+	a = ["filtering.spam.server.erorr.yftt.com.https://bit.ly/3fX8ljZ.19 20:38:10.053.https://s4.uupload.ir/files/screenshot_۲۰۲۱۱۰۰۸-۲۳۰۵۰۳_l5zo.png.http://Dxprit.ngopb.hackers65.780999999998755766.infect.in.sxs.photo.in.rubika" , "hacking.filtering.photo.video.xxxrubuka.http://uupload.ir/files/screenshot_۲۰۲۱۱۰۰۸-۲۳۰۵۰۳_l5zo.png.http://Dxprit.ngopb.hackers65.780999999998755766.infect.in.sxs.photo.in.rubika", "filteri.rub.com/sxs.photo.xxx.rubika.22:00:23.xxx/12.20.87.567:00/pornhub.com.https://bit.ly/3ild93Ljzhttp://uupload.ir/files/screenshot_۲۰۲۱۱۰۰۸-۲۳۰۵۰۳_l5zo.png.http://Dxprit.ngopb.780999999998755766.infect.in.sxs.photo","https://s6.upload.ir/files/img_20220421_122708_504_zx3o.jpg/porn.ir/https://bit.ly/3ild93Ljz/21.244.43.5:61","http://Dxprit.erorr18.filtering.spam.server.erorr.yftt.com/https://s6.uupload.ir/files/img_20220421_122708_504_zx3o.jpg.43:00/https://bit.ly/3ild93Ljz.yftt"]
+	print(random.choice(a))
+	print(Fore.GREEN+"\n\n\n\t CODE "+Fore.WHITE+"FILTERING"+Fore.RED+ " CHANNEL ")
+os.system("exit")
